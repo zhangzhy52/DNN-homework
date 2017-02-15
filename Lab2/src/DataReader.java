c56089bb36e04a966165a53c4eed79ca21917b11
zhenyu zhang <zhenyu zhang>
2017-02-16 01:55:30 +0800

    change_splitDataset

--- Lab2/src/DataReader.java
+++ Lab2/src/DataReader.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+//import java.util.StringJoiner;
 
 /**
  * Created by zhicheng on 1/23/17.
@@ -11,16 +12,22 @@ public class DataReader {
 
     public int numFeatures = 20;
     public int numLabels = 0;
-    public int numExamples = -1;
+    //public int numExamples = -1;
     public int numProteins = 0;
     public int winSize = 17;
 
     public ArrayList<String> features;
     public ArrayList<String> labels;
-    public ArrayList<ArrayList<String[]>> examples;
+    public ArrayList<ArrayList<String[]>> trainExamples;
+    public ArrayList<ArrayList<String[]>> tuneExamples;
+    public ArrayList<ArrayList<String[]>> testExamples;
 
-    public ArrayList<ArrayList<Double>> data;
-    public ArrayList<ArrayList<Double>> data_label;
+    public ArrayList<ArrayList<Double>> trainData;
+    public ArrayList<ArrayList<Double>> train_data_label;
+    public ArrayList<ArrayList<Double>> tuneData;
+    public ArrayList<ArrayList<Double>> tune_data_label;
+    public ArrayList<ArrayList<Double>> testData;
+    public ArrayList<ArrayList<Double>> test_data_label;
 
     DataReader() {
     }
@@ -41,7 +48,15 @@ public class DataReader {
             {
                 numProteins++;
                 if (!tmp.isEmpty())
-                    examples.add(tmp);
+                {
+                    if (numProteins %5==0)
+                        tuneExamples.add(tmp);
+                    else if (numProteins %5 ==1) {
+                        testExamples.add(tmp);
+                    }else {
+                        trainExamples.add(tmp);
+                    }
+                }
                 tmp = new ArrayList<>();
                 continue;
             }
@@ -54,31 +69,9 @@ public class DataReader {
         numFeatures = features.size() + 1; // add one more for padding,let it be [0,0,0,...1]
         numLabels = labels.size();
         int halfWindow = (winSize + 1)/2;
-
-        for (ArrayList<String[]> example : examples) {
-            for (int e = 0; e < example.size(); e++){
-
-                //int f_index = features.indexOf(example.get(e)[0]);
-                int l_index = labels.indexOf(example.get(e)[1]);
-                data_label.add(onehotCoding(l_index, numLabels));
-                ArrayList<Double> tmp_feature = new ArrayList<>(); // For ading data_feature
-                // padding number is  win/2 - e
-
-                for (int w = e - halfWindow + 1; w < e + halfWindow; w++){
-
-                    if ( w < 0 || w >= example.size())
-                        tmp_feature.addAll(onehotCoding(numFeatures - 1, numFeatures));
-                    else
-                    {
-                        tmp_feature.addAll(onehotCoding(features.indexOf(example.get(w)[0]), numFeatures));
-                    }
-                }
-                data.add(tmp_feature);
-
-            }
-
-        }
-
+        getDataFeatureLabel(trainExamples, trainData, train_data_label, halfWindow);
+        getDataFeatureLabel(tuneExamples, tuneData, tune_data_label, halfWindow);
+        getDataFeatureLabel(testExamples, testData, test_data_label, halfWindow);
     }
 
     private ArrayList<Double> onehotCoding(int index, int size)
@@ -93,14 +86,51 @@ public class DataReader {
         return onehot;
     }
 
+
+    private void getDataFeatureLabel(ArrayList<ArrayList<String[]>> examples,
+                                     ArrayList<ArrayList<Double>> data, ArrayList<ArrayList<Double>> data_label,
+                                     int halfWindow)
+    {
+        for (ArrayList<String[]> example : examples) {
+            for (int e = 0; e < example.size(); e++){
+
+                //int f_index = features.indexOf(example.get(e)[0]);
+                int l_index = labels.indexOf(example.get(e)[1]);
+                data_label.add(onehotCoding(l_index, numLabels));
+                ArrayList<Double> tmp_feature = new ArrayList<>(); // For ading data_feature
+                // padding number is  win/2 - e
+
+                for (int w = e - halfWindow; w < e + halfWindow; w++){
+
+                    if ( w < 0 || w >= example.size())
+                        tmp_feature.addAll(onehotCoding(numFeatures - 1, numFeatures));
+                    else
+                    {
+                        tmp_feature.addAll(onehotCoding(features.indexOf(example.get(w)[0]), numFeatures));
+                    }
+                }
+                data.add(tmp_feature);
+
+            }
+
+        }
+    }
+
+
     private void init() {
         numFeatures = -1;
         //numExamples = -1;
         features = new ArrayList<>();
         labels = new ArrayList<>();
-        examples = new ArrayList<>();
-        data = new ArrayList<>();
-        data_label = new ArrayList<>();
+        trainExamples = new ArrayList<>();
+        tuneExamples = new ArrayList<>();
+        testExamples = new ArrayList<>();
+        trainData = new ArrayList<>();
+        tuneData = new ArrayList<>();
+        testData = new ArrayList<>();
+        train_data_label = new ArrayList<>();
+        tune_data_label = new  ArrayList<>();
+        test_data_label = new ArrayList<>();
     }
 
 
